cbf3d65012df5d779dfa313c705dffbc59caffaa
rafglo <276020@student.pwr.edu.pl>
2024-04-02 14:01:37 +0200

    lista 3
    zad8,9

--- test.py
+++ test.py
@@ -1,15 +1,29 @@
-def median(arr):
-    n = len(arr)
-    arr.sort()
-    if n % 2 == 1:
-        return arr[n//2]
-    else:
-        return (arr[int(n/2) - 1] + arr[int(n/2)])/2
+dyrektor = [41,27,35,33,25,47,38,53,43,35,36]
+wizytator = [38,24,34,29,27,47,43,52,39,31,29]
 
-def Q1(arr):
-    n = len(arr)
-    less_than = arr[n // 2:]
-    print(less_than)
-    return median(less_than)
+def nadawanie_rang(wartosci):
+    wartosci_sorted = wartosci.copy()
+    wartosci_sorted.sort(reverse=True)
+    rangi = [0]*len(wartosci)
+    for i in range(len(wartosci_sorted)):
+        ile = wartosci.count(wartosci_sorted[i]) 
+        if ile == 1:
+            ind = wartosci.index(wartosci_sorted[i])
+            rangi[ind] = i + 1
+        else:
+            ind_sum = ile
+            inds = []
+            for j in range(len(wartosci)):
+                if wartosci_sorted[i] == wartosci[j]:
+                    inds.append(j)
 
-print(Q1([3,4,5,6,7,4,67,7,4]))
+            for k in range(len(wartosci_sorted)):
+                if wartosci_sorted[i] == wartosci_sorted[k]:
+                    ind_sum += k
+                    
+            ranga = ind_sum/ile
+            for ind in inds:
+                rangi[ind] = ranga
+    return rangi
+
+print(nadawanie_rang(dyrektor))
